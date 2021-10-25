25a7e4d675a4235fa7482d274bb2e2090a30f054
Emmanuel Etti <emmanueletti@gmail.com>
2021-10-25 08:59:49 -0400

    update

--- lib/main.dart
+++ lib/main.dart
@@ -3,44 +3,73 @@ import 'package:flutter/material.dart';
 // Note:
 // all widgets are classes that extend a base widget in Flutter
 // the classes will have a build method that returns the widget
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  // createState method returns a State with type of StatefulWidget
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+// state object that will be returned by MyApp
+class _MyAppState extends State<MyApp> {
+  final _questions = [
+    "Whats your favourite colour?",
+    "What's your favourite animal?"
+  ];
+
+  // using var instead of the type "int" as it is better practise to
+  // rely on type inference for simple things like this,
+  // don't duplicate work
+  var _questionIndex = 0;
+
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex++;
+    });
+    print(_questionIndex);
+    print('answer chosen');
+  }
+
+// build method that returns a widget
   @override
   Widget build(BuildContext context) {
-    void answerQuestion() {
-      print('answer chosen');
-    }
-
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: Column(
-          children: <Widget>[
-            Text('The Question'),
-            ElevatedButton(
-              child: Text('Answer 1'),
-              // can add callback function for event listener:
-              // 1 - as a named function
-              onPressed: answerQuestion,
-            ),
-            ElevatedButton(
-              child: Text('Answer 2'),
-              // 2 - as an anonymous function in arrow syntax
-              // if only one line of expression to execute
-              onPressed: () => print('answer chosen'),
-            ),
-            ElevatedButton(
-              child: Text('Answer 3'),
-              // 3 - as an anonymous function with more lines of expression
-              // invovled
-              onPressed: () {
-                // ...code
-                print('answer chosen');
-              },
-            ),
-          ],
-        ),
+        // named body argument of the Scaffold class takes a column widget
+        // which itself takes a named children argument that takes an array
+        // of widgets
+        body: Column(children: <Widget>[
+          Text(_questions[_questionIndex]),
+          RaisedButton(
+            child: Text('Answer 1'),
+            // can add callback function for event listener:
+            // 1 - as a named function
+            onPressed: _answerQuestion,
+          ),
+          RaisedButton(
+            child: Text('Answer 2'),
+            // 2 - as an anonymous function in arrow syntax
+            // if only one line of expression to execute
+            // anonymous function format only used when you won't need
+            // to re-use the function
+            // onPressed: () => print('answer chosen'),
+            onPressed: _answerQuestion,
+          ),
+          RaisedButton(
+            child: Text('Answer 3'),
+            // 3 - as an anonymous function with more lines of expression
+            // invovled
+            // onPressed: () {
+            //   // ...code
+            //   print('answer chosen');
+            // }
+            onPressed: _answerQuestion,
+          ),
+        ]),
       ),
     );
   }
